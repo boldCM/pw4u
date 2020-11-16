27dafe7ebb71c7a2edfa9fa0c5c7dc957199aa44
boldCM <carolin@cc-meier.de>
2020-11-16 15:39:50 +0100

    Add error message on getPassword

--- server.js
+++ server.js
@@ -3,15 +3,22 @@ require("dotenv").config();
 const express = require("express");
 const { getPassword, deleteItem } = require("./lib/passwords");
 const { connect } = require("./lib/database");
+const { response } = require("express");
 
 const app = express();
 const port = 3600;
 
 app.get("/api/passwords/:name", async (request, response) => {
   const { name } = request.params;
+
+  try{
   const passwordValue = await getPassword(name);
   response.send(passwordValue);
-});
+}
+catch (error) {
+    console.error(error);
+    response.status(404).send("Could not find the password you have specified")
+}
 
 app.post("/api/passwords", (request, response) => {
   response.send("under construction");
