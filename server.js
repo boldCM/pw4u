8bd63511189d43668ddac691b60774ec56ca06cd
boldCM <carolin@cc-meier.de>
2020-11-16 15:59:32 +0100

    Add POST method and more try and catch functionality

--- server.js
+++ server.js
@@ -1,27 +1,43 @@
 require("dotenv").config();
 
 const express = require("express");
-const { getPassword, deleteItem } = require("./lib/passwords");
+const { getPassword, deleteItem, setPassword } = require("./lib/passwords");
 const { connect } = require("./lib/database");
 const { response } = require("express");
 
 const app = express();
+app.use(express.json());
 const port = 3600;
 
 app.get("/api/passwords/:name", async (request, response) => {
   const { name } = request.params;
 
-  try{
-  const passwordValue = await getPassword(name);
-  response.send(passwordValue);
-}
-catch (error) {
+  try {
+    const passwordValue = await getPassword(name);
+    if (!passwordValue) {
+      response
+        .status(404)
+        .send("Could not find the password you have specifed");
+      return;
+    }
+    response.send(passwordValue);
+  } catch (error) {
     console.error(error);
-    response.status(404).send("Could not find the password you have specified")
-}
+    response.status(500).send("An internal server error occured");
+  }
+});
 
-app.post("/api/passwords", (request, response) => {
-  response.send("under construction");
+app.post("/api/passwords", async (request, response) => {
+  const password = request.body;
+  try {
+    await setPassword(password.name, password.value);
+    response.send(`Successfully set ${password.name}`);
+  } catch (error) {
+    console.error(error);
+    response
+      .status(500)
+      .send("An unexpected error occured. Please try again later!");
+  }
 });
 
 app.delete("/api/passwords/:name", async (request, response) => {
