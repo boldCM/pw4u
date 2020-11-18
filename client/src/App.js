261d97dfe32641e6ac4664964a6f21084d481ba5
boldCM <carolin@cc-meier.de>
2020-11-18 14:34:04 +0100

    Working Input and Outputfield without onChange

--- client/src/App.js
+++ client/src/App.js
@@ -6,23 +6,34 @@ import { useEffect, useState } from "react";
 function App() {
   const [password, setPassword] = useState(null);
   const [inputValue, setInputValue] = useState("");
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    const doFetch = async () => {
+  const doFetch = async () => {
+    try {
+      setLoading(true);
+      setError(null);
       const newPassword = await getPassword(inputValue);
       setPassword(newPassword);
-    };
-    doFetch();
-  }, [inputValue]);
+    } catch (error) {
+      console.error(error);
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {error && <div>{error.message}</div>}
         <form
           onSubmit={(event) => {
             event.preventDefault();
-            getPassword(inputValue);
+            // setInputValue(event.target.value);
+            // console.log(inputValue);
+            doFetch();
             setInputValue("");
           }}
         >
@@ -30,10 +41,12 @@ function App() {
             type="text"
             placeholder="search for password"
             value={inputValue}
+            required={true}
             onChange={(event) => setInputValue(event.target.value)}
-          ></input>
+          />
         </form>
-        <div>{inputValue && password}</div>
+        {loading && <div>Loading...</div>}
+        <div>{password}</div>
       </header>
     </div>
   );
