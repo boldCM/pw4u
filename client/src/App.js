45e76384684b1b73337754d7e233b2b1b1fdeafc
boldCM <carolin@cc-meier.de>
2020-11-18 12:31:44 +0100

    Add Inputfield and OUtputfield

--- client/src/App.js
+++ client/src/App.js
@@ -5,20 +5,35 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [password, setPassword] = useState(null);
+  const [inputValue, setInputValue] = useState("");
 
   useEffect(() => {
     const doFetch = async () => {
-      const newPassword = await getPassword("wifi");
+      const newPassword = await getPassword(inputValue);
       setPassword(newPassword);
     };
     doFetch();
-  }, []);
+  }, [inputValue]);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {password}
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            getPassword(inputValue);
+            setInputValue("");
+          }}
+        >
+          <input
+            type="text"
+            placeholder="search for password"
+            value={inputValue}
+            onChange={(event) => setInputValue(event.target.value)}
+          ></input>
+        </form>
+        <div>{inputValue && password}</div>
       </header>
     </div>
   );
