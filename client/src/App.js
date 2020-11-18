d7682e249fad692a10f6a7a5ceb98a25d9220c00
boldCM <carolin@cc-meier.de>
2020-11-18 11:14:24 +0100

    Creat React App and get Password, optimize Prxy and concurrently

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { getPassword } from "./api/passwords";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [password, setPassword] = useState(null);
+
+  useEffect(() => {
+    const doFetch = async () => {
+      const newPassword = await getPassword("wifi");
+      setPassword(newPassword);
+    };
+    doFetch();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {password}
+      </header>
+    </div>
+  );
+}
+
+export default App;
