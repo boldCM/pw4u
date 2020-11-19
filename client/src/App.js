15a286b59ca64cc2c072075326b996bc9d997e18
boldCM <carolin@cc-meier.de>
2020-11-19 14:01:25 +0100

    Try react forms

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { getPassword } from "./api/passwords";
 import { useState } from "react";
 import useAsync from "./hooks/useAsync";
+import { useForm } from "react-hook-form";
 
 function App() {
   const [inputValue, setInputValue] = useState("");
@@ -11,8 +12,25 @@ function App() {
     getPassword(inputValue)
   );
 
+  const { register, handleSubmit, watch, errors } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  console.log(watch("example"));
+
   return (
     <div className="App">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* register your input into the hook by invoking the "register" function */}
+        <input name="example" defaultValue="test" ref={register} />
+
+        {/* include validation with required or other standard HTML validation rules */}
+        <input name="exampleRequired" ref={register({ required: true })} />
+        {/* errors will return when field validation fails  */}
+        {errors.exampleRequired && <span>This field is required</span>}
+
+        <input type="submit" />
+      </form>
+      {/* ursprünglicher Code: */}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         {error && <div>{error.message}</div>}
